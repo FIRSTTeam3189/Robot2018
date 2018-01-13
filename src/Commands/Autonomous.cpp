1e9b20f1e79853a9d5c550985608a1653e7b6482
Devlyn Nelson <devlynknelson@gmail.com>
2018-01-13 13:52:35 -0800

    added auto side data

--- src/Commands/Autonomous.cpp
+++ src/Commands/Autonomous.cpp
@@ -1,7 +1,17 @@
 #include "Autonomous.h"
 
 #include <iostream>
+#include <DriverStation.h>
 
 Autonomous::Autonomous() :
 		frc::CommandGroup("Autonomous") {
+
+	std::string state = DriverStation::GetInstance().GetGameSpecificMessage();
+
+	if(state[0] == 'L'){
+
+	}else{
+
+	}
+
 }
