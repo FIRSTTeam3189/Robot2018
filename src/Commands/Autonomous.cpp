369246991b20436f3a910474e1dd4f5b251d4b1b
Devlyn Nelson <devlynknelson@gmail.com>
2018-01-13 13:59:40 -0800

    added comments

--- src/Commands/Autonomous.cpp
+++ src/Commands/Autonomous.cpp
@@ -6,6 +6,9 @@
 Autonomous::Autonomous() :
 		frc::CommandGroup("Autonomous") {
 
+	// the data is three chars defining side of objects from you perspective
+	// (L = Left, R = Right, CAPS ARE IMPORTANT) Going closest to farthest
+	// ex: "LLR" our switch is first being left from our driverstaion
 	std::string state = DriverStation::GetInstance().GetGameSpecificMessage();
 
 	if(state[0] == 'L'){
